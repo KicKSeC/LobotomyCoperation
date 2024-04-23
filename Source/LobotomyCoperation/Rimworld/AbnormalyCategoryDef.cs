09035161243093d6b9cd19063d8d5c035559a1da
JunHyeong Park <wist2483@gmail.com>
2024-04-24 03:53:24 +0900

    Add Abnormaly related Classes

---
+++ Source/LobotomyCoperation/Rimworld/AbnormalyCategoryDef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace LobotomyCoperation.Rimworld
+{
+    public sealed class AbnormalyCategoryDef : Def
+    {
+        public int listOrder = 999999;
+    }
+}
