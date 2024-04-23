9067d523d2d17bdd3026fc1482b7187e1c7f984f
JunHyeong Park <wist2483@gmail.com>
2024-04-24 03:12:54 +0900

    add nothing there

---
+++ Source/LobotomyCoperation/ExtractAbnormaly.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using Verse.AI.Group;
+using Verse.Sound;
+
+namespace LobotomyCoperation
+{
+    public class ExtractAbnormaly : PsychicRitualToil_VoidProvocation
+    {
+
+    }
+}
